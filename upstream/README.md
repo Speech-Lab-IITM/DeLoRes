80ef679e50b64b7fe9e8d642abdce0ca4e2c0de2
Sreyan Ghosh <gsreyan@gmail.com>
2021-10-15 13:20:47 +0530

    barlow_upstream

---
+++ upstream/README.md
@@ -0,0 +1,14 @@
+# Deep_cluster
+
+
+# Upstream Training
+```
+python main.py \
+    --input "path_to_input_csv" \
+    --epochs 200 \
+    --batch_size 64 \
+    --cluster_algo kmeans \
+    --num_clusters 512 \
+    --save_dir "path_to_dir_to_be_saved" \
+    --num_workers 2
+```
---
+++ upstream/datasets.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+import torch.utils.data as data
+import tensorflow as tf
+from torch.utils.data import Dataset
+import librosa
+
+from utils import extract_log_mel_spectrogram, extract_window
+
+
+tf.config.set_visible_devices([], 'GPU')
+AUDIO_SR = 16000
+
+
+def collate_fn_padd(batch):
+    '''
+    Padds batch of variable length
+
+    note: it converts things ToTensor manually here since the ToTensor transform
+    assume it takes in images rather than arbitrary tensors.
+    '''
+    ## padd
+    batch_1 = [torch.Tensor(t) for t,_ in batch]
+    batch_1 = torch.nn.utils.rnn.pad_sequence(batch_1,batch_first = True)
+    #batch = batch.reshape()
+    batch_1 = batch_1.unsqueeze(1)
+
+    batch_2 = [torch.Tensor(t) for _,t in batch]
+    batch_2 = torch.nn.utils.rnn.pad_sequence(batch_2,batch_first = True)
+    #batch = batch.reshape()
+    batch_2 = batch_2.unsqueeze(1)
+
+    return batch_1, batch_2
+
+
+class BARLOW(Dataset):
+
+    def __init__(self, data_dir_list):
+        self.audio_files_list = data_dir_list
+
+    def __getitem__(self, idx):
+        audio_file = self.audio_files_list[idx]
+        wave,sr = librosa.core.load(audio_file, sr=AUDIO_SR)
+        x = tf.math.l2_normalize(wave, epsilon=1e-9)
+
+        waveform_a = extract_window(x)
+        log_mel_spec_a = extract_log_mel_spectrogram(waveform_a)
+
+        waveform_b = extract_window(x)
+        log_mel_spec_b = extract_log_mel_spectrogram(waveform_b)
+
+        return log_mel_spec_a.numpy() , log_mel_spec_b.numpy()
+
+    def __len__(self):
+        return len(self.audio_files_list)

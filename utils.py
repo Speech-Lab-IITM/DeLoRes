ae80bf413dbf4a31b2dbf037f2c3990b363192e6
cs20s030 <89025294+cs20s030@users.noreply.github.com>
2021-10-30 10:09:39 +0530

    changed utils.py

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ def str2bool(v):
 def get_downstream_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--down_stream_task', default="iemocap", type=str,
-                        help='''down_stream task name one of 
+                        help='''down_stream task name one of
                         birdsong_freefield1010 , birdsong_warblr ,
                         speech_commands_v1 , speech_commands_v2
                         libri_100 , musical_instruments , iemocap , tut_urban , voxceleb1 , musan
@@ -37,22 +37,22 @@ def get_downstream_parser():
     parser.add_argument('--resume', default = False, type=str2bool,
                         help='number of total epochs to run')
     parser.add_argument('--pretrain_path', default=None, type=Path,
-                        help='Path to Pretrain weights') 
+                        help='Path to Pretrain weights')
     parser.add_argument('--freeze_effnet', default=True, type=str2bool,
-                        help='Path to Pretrain weights')  
+                        help='Path to Pretrain weights')
     parser.add_argument('--final_pooling_type', default='Avg', type=str,
-                        help='valid final pooling types are Avg,Max')                                                            
-    parser.add_argument('--load_only_efficientNet',default = True,type =str2bool)  
+                        help='valid final pooling types are Avg,Max')
+    parser.add_argument('--load_only_efficientNet',default = True,type =str2bool)
     parser.add_argument('--tag',default = "pretrain_big",type =str)
-    parser.add_argument('--exp-dir',default='./exp/',type=Path,help="experiment root directory")    
-    parser.add_argument('--lr',default=0.001,type=float,help="experiment root directory")                    
+    parser.add_argument('--exp-dir',default='./exp/',type=Path,help="experiment root directory")
+    parser.add_argument('--lr',default=0.001,type=float,help="experiment root directory")
     return parser
 
 
 def freeze_effnet(model):
     logger=logging.getLogger("__main__")
     logger.info("freezing effnet weights")
-    for param in model.model_efficient.parameters():
+    for param in model.module.model_efficient.parameters():
         param.requires_grad = False
 
 def load_pretrain(path,model,
@@ -71,9 +71,10 @@ def load_pretrain(path,model,
     logger.info("Model unexpected keys")
     logger.info(mod_unexpected_keys)
     print(mod_unexpected_keys)
-    if freeze_effnet : 
+    if freeze_effnet :
+        print('freeze')
         logger.info("freezing effnet weights")
-        for param in model.model_efficient.parameters():
+        for param in model.module.model_efficient.parameters():
             param.requires_grad = False
     logger.info("done loading")
     return model
@@ -82,9 +83,9 @@ def resume_from_checkpoint(path,model,optimizer):
     logger = logging.getLogger("__main__")
     logger.info("loading from checkpoint : "+path)
     checkpoint = torch.load(path)
-    start_epoch = checkpoint['epoch']  
+    start_epoch = checkpoint['epoch']
     logger.info("Task :: {}".format(checkpoint['down_stream_task']))
-    mod_missing_keys,mod_unexpected_keys = model.load_state_dict(checkpoint['state_dict'],strict=False)  
+    mod_missing_keys,mod_unexpected_keys = model.load_state_dict(checkpoint['state_dict'],strict=False)
     opt_missing_keys,opt_unexpected_keys = optimizer.load_state_dict(checkpoint['optimizer'])
     logger.info("Model missing keys",mod_missing_keys)
     logger.info("Model unexpected keys",mod_unexpected_keys)
@@ -133,7 +134,7 @@ class Metric(object):
         if isinstance(val, (torch.Tensor)):
             val = val.numpy()
             self.val = val
-            self.sum += np.sum(val) 
+            self.sum += np.sum(val)
             self.count += np.size(val)
         self.avg = self.sum / self.count
 
@@ -153,4 +154,3 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-

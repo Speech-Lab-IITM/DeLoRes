9b83e8b9558741e3088b860e20bace0dddc59888
cs20s030 <89025294+cs20s030@users.noreply.github.com>
2021-10-30 08:35:36 +0530

    Add files via upload

--- main_downstream.py
+++ main_downstream.py
@@ -24,7 +24,7 @@ def get_logger(args):
 
 
 def main_worker(gpu, args):
-    
+
     args.rank += gpu
     torch.distributed.init_process_group(
         backend='nccl', init_method=args.dist_url,
@@ -40,21 +40,21 @@ def main_worker(gpu, args):
         print(' '.join(sys.argv), file=stats_file)
     logger = get_logger(args)
     torch.cuda.set_device(gpu)
-    torch.backends.cudnn.benchmark = True # ! change it set seed 
+    torch.backends.cudnn.benchmark = True # ! change it set seed
 
-    # train and test loaders 
-    # ! user sampler and ddp 
+    # train and test loaders
+    # ! user sampler and ddp
     assert args.batch_size % args.world_size == 0
     per_device_batch_size = args.batch_size // args.world_size
 
     train_dataset,test_dataset = get_dataset(args.down_stream_task)
-    
-    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, shuffle=True, seed=1)    
-    
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, shuffle=True, seed=1)
+
     train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=per_device_batch_size,
                                                 collate_fn = DataUtils.collate_fn_padd_2,
                                                 pin_memory=True,sampler = train_sampler)
-    
+
     test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=args.batch_size,
                                                 collate_fn = DataUtils.collate_fn_padd_2,
                                                 pin_memory=True)
@@ -63,8 +63,11 @@ def main_worker(gpu, args):
     model = DownstreamClassifer(no_of_classes=train_dataset.no_of_classes,
                                 final_pooling_type=args.final_pooling_type).cuda(gpu)
 
-    model = nn.SyncBatchNorm.convert_sync_batchnorm(model) 
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    #if args.freeze_effnet:
+    #    freeze_effnet(model)
+
+    model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu], find_unused_parameters=True) #I have to use this (find_unused_parameters=Ture)
 
     # Resume
     start_epoch =0
@@ -73,30 +76,29 @@ def main_worker(gpu, args):
         resume_from_checkpoint(args.pretrain_path,model,optimizer)
     elif args.pretrain_path:
         logger.info("pretrain Weights init")
-        load_pretrain(args.pretrain_path,model,args.load_only_efficientNet,args.freeze_effnet)
+        load_pretrain(args.pretrain_path,model,args.load_only_efficientNet,args.freeze_effnet) #this function also uses freeze code
     else:
         logger.info("Random Weights init")
     # Freeze effnet
-    if args.freeze_effnet:
-        freeze_effnet(model)
-    
+    #if args.freeze_effnet:
+    #    freeze_effnet(model)
     criterion = nn.CrossEntropyLoss().cuda(gpu)
     optimizer = torch.optim.Adam(
         filter(lambda x: x.requires_grad, model.parameters()),
         lr=args.lr,
     )
-    
+
     if args.rank == 0 : logger.info("started training")
-    
+
     train_accuracy = []
     train_losses=[]
     test_accuracy = []
     test_losses=[]
-    
+
     for epoch in range(start_epoch,args.epochs):
         train_sampler.set_epoch(epoch)
         train_stats = train_one_epoch(train_loader, model, criterion, optimizer, epoch,gpu,args)
-        
+
         # save_to_checkpoint(args.down_stream_task,args.exp_root,epoch,model,optimizer)
 
         if args.rank == 0 :
@@ -108,7 +110,7 @@ def main_worker(gpu, args):
             stats = dict(epoch=epoch,
                     Train_loss=train_stats["loss"].avg.cpu().numpy().item(),
                     Test_Loss=(eval_stats["loss"].avg).numpy().item(),
-                    Test_Accuracy =eval_stats["accuracy"].avg,  
+                    Test_Accuracy =eval_stats["accuracy"].avg,
                     Best_Test_Acc=max(test_accuracy))
             print(stats)
             print(json.dumps(stats), file=stats_file)
@@ -131,14 +133,14 @@ def train_one_epoch(loader, model, crit, opt, epoch,gpu,args):
     losses = AverageMeter()
     data_time = AverageMeter()
 
-    model.train() # ! imp 
+    model.train() # ! imp
     end = time.time()
     for i, (input_tensor, target) in enumerate(loader):
         data_time.update(time.time() - end)
 
         output = model(input_tensor.cuda(gpu, non_blocking=True))
         loss = crit(output, target.cuda(gpu, non_blocking=True))
-        
+
         losses.update(loss.data, input_tensor.size(0))
         opt.zero_grad()
         loss.backward()
@@ -153,8 +155,8 @@ def train_one_epoch(loader, model, crit, opt, epoch,gpu,args):
                     'Loss: {loss.val:.4f} ({loss.avg:.4f})'
                     .format(epoch, i, len(loader), batch_time=batch_time,
                             data_time=data_time, loss=losses))
-    
-    
+
+
     logger.debug("epoch-"+str(epoch) +" ended")
     stats = dict(epoch=epoch,loss=losses)
     return stats
@@ -162,21 +164,21 @@ def train_one_epoch(loader, model, crit, opt, epoch,gpu,args):
 def eval(epoch,model,loader,crit,args,gpu,stats_file):
     model.eval() # ! Imp
     losses = AverageMeter()
-    accuracy = Metric() 
+    accuracy = Metric()
     with torch.no_grad():
         for step, (input_tensor, targets) in enumerate(loader):
-            input_tensor = torch.squeeze(input_tensor,0)
+           # input_tensor = torch.squeeze(input_tensor,0)
             if torch.cuda.is_available():
                 input_tensor =input_tensor.cuda(gpu ,non_blocking=True)
                 targets = targets.cuda(gpu,non_blocking=True)
             with torch.cuda.amp.autocast():
-                outputs = model(input_tensor) 
+                outputs = model(input_tensor)
                 loss = crit(outputs, targets)
                 preds = torch.argmax(outputs,dim=1)==targets
 
             accuracy.update(preds.cpu())
             losses.update(loss.cpu().data, input_tensor.size(0))
-    
+
     stats = dict(epoch=epoch,loss=losses, accuracy = accuracy)
     return stats
 
@@ -184,10 +186,10 @@ def main():
     parser=get_downstream_parser()
     args = parser.parse_args()
     args.ngpus_per_node = torch.cuda.device_count()
-    
+
     # single-node distributed training
     args.rank = 0
-    args.dist_url = 'tcp://localhost:58362'
+    args.dist_url = 'tcp://localhost:58364'
     args.world_size = args.ngpus_per_node
     torch.multiprocessing.spawn(main_worker, (args,), args.ngpus_per_node)
 

6d23e60c0a18310fe333fecb71488b44d1b56134
cs20s030 <89025294+cs20s030@users.noreply.github.com>
2021-11-07 02:06:44 +0530

    only change for tut urban

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -5,27 +5,26 @@ from datasets.speech_commands_v2_avg import SpeechCommandsV2Train, SpeechCommand
 from datasets.musical_instruments_avg import MusicalInstrumentsTrain, MusicalInstrumentsTest
 import torch
 
-def get_dataset(downstream_task_name):
+def get_dataset(downstream_task_name, aug = None):
     if downstream_task_name == "birdsong_combined":
         return BirdSongDatasetTrain(), BirdSongDatasetTest()
 
-                 
+
     # elif downstream_task_name == "speech_commands_v1":
     #     return SpeechCommandsV1Train() , SpeechCommandsV1Test()
     elif downstream_task_name == "speech_commands_v2":
         return SpeechCommandsV2Train() , SpeechCommandsV2Test()
     # elif downstream_task_name == "libri_100":
-    #     return Libri100Train() , Libri100Test()      
+    #     return Libri100Train() , Libri100Test()
     elif downstream_task_name == "musical_instruments":
         return MusicalInstrumentsTrain() , MusicalInstrumentsTest()
     elif downstream_task_name == "iemocap":
-        return IEMOCAPTrain(),IEMOCAPTest()            
+        return IEMOCAPTrain(),IEMOCAPTest()
     elif downstream_task_name == "tut_urban":
-        return TutUrbanSoundsTrain(),TutUrbanSoundsTest()    
+        return TutUrbanSoundsTrain(tfms = aug),TutUrbanSoundsTest(tfms = aug)
     # elif downstream_task_name == "voxceleb_v1":
-    #     return Voxceleb1Train() , Voxceleb1DatasetTest()   
+    #     return Voxceleb1Train() , Voxceleb1DatasetTest()
     # elif downstream_task_name == "language_identification":
-    #     return LanguageIdentificationTrain(), LanguageIdentificationTest()                 
+    #     return LanguageIdentificationTrain(), LanguageIdentificationTest()
     else:
         raise NotImplementedError
-
